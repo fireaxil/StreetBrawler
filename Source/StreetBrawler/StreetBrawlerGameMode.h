2befa130e5e11a3811bd58fc382bd98784a5d4d9
Christian Koehler <sandvice@gmail.com>
2015-10-11 15:45:26 -0400

    second commit

---
+++ Source/StreetBrawler/StreetBrawlerGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "StreetBrawlerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STREETBRAWLER_API AStreetBrawlerGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
